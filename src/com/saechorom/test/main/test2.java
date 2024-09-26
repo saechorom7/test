7f85cb53138b0691236bb7bf7188d7ff6404a21c
saechorom7 <jennyfive@naver.com>
2024-09-26 21:16:00 +0900

    yujin test2 class 생성

---
+++ src/com/saechorom/test/main/test2.java
@@ -0,0 +1,11 @@
+package com.saechorom.test.main;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("test2");
+		int a = 10;
+		System.out.println(a);
+	}
+
+}
